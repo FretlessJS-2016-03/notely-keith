d6fb1b37fc672c4c0fef26959e6e60f3221b3070
Keith Turner <turner@keithturner.com>
2016-03-30 11:51:53 -0600

    Retrieve notes from MongoDB.

--- server/app.js
+++ server/app.js
@@ -1,23 +1,26 @@
 var express = require('express');
 var notelyServerApp = express();
 
-//cross origin Resource Sharing (CORS) middleware
-notelyServerApp.use(function(req, res, next){
-res.header('Access-Control-Allow-Origin','*');
-next();
+var db = require('mongoose');
+db.connect('mongodb://localhost:27017/notely');
+
+var NoteSchema = db.Schema({
+  title: String,
+  body_html: String
+});
+
+var Note = db.model('Note', NoteSchema);
+
+// Cross-Origin Resource Sharing (CORS) middleware
+notelyServerApp.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
 });
 
 notelyServerApp.get('/', function(req, res) {
-  res.json([
-    {
-      title: 'Hardcoded note',
-      body_html: 'Cool note. Aww, shucks.'
-    },
-    {
-      title: 'Another hardcoded note',
-      body_html: "Ain't life grand?"
-    }
-  ]);
+  Note.find().then(function(notes) {
+    res.json(notes);
+  });
 });
 
 notelyServerApp.listen(3030, function() {
