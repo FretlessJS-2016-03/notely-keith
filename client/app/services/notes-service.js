cffc7ec657e3633dec85e8534cfa6cd8a76b6161
Keith Turner <turner@keithturner.com>
2016-03-31 12:52:39 -0600

    Keep Sidebar in sync.

--- client/app/services/notes-service.js
+++ client/app/services/notes-service.js
@@ -34,6 +34,26 @@
       });
     };
 
+    _this.update = function(note) {
+      return $http.put('http://localhost:3030/notes/' + note._id, {
+        note: {
+          title: note.title,
+          body_html: note.body_html
+        }
+      }).then(function(response) {
+        _this.replaceNote(response.data.note);  // making this work
+      });
+    };
+
+    _this.replaceNote = function(updatedNote) {
+      for (var i = 0; i < _this.notes.length; i++) {
+        if (_this.notes[i]._id === updatedNote._id) {
+          _this.notes[i] = updatedNote;
+          return;
+        }
+      }
+    };
+
     _this.findById = function(noteId) {
       for (var i = 0; i < _this.notes.length; i++) {
         // If the IDs match, return the current note
